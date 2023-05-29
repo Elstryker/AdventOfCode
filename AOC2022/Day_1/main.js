ee78927a5cf4427be1b7df0fe08c27832d7e7900
Elstryker <rubencorreiacerqueira@gmail.com>
2023-05-29 22:40:12 +0100

    add AOC2022 day 2

--- AOC2022/Day_1/main.js
+++ AOC2022/Day_1/main.js
@@ -1,35 +1,35 @@
-import { log } from "console";
-import * as help from "../Common/help.js";
+import { log } from 'console';
+import * as help from '../Common/help.js';
+
+const groupElfCalories = (caloriesString) => {
+  const allCalories = caloriesString.split('\n');
+  const allCaloriesNums = allCalories.map((x) => Number.parseInt(x, 10));
+  const summedCalories = allCaloriesNums.reduce((acc, current) => acc + current);
+  return summedCalories;
+};
 
 export function Day1Part1() {
   // let inp = help.readShortInputByDay(1)
-  let inp = help.readInputByDay(1);
+  const inp = help.readInputByDay(1);
 
-  let elvesCalories = inp.split("\n\n");
+  let elvesCalories = inp.split('\n\n');
 
   elvesCalories = elvesCalories.map(groupElfCalories);
 
-  let mostCalories = Math.max(...elvesCalories);
+  const mostCalories = Math.max(...elvesCalories);
 
-  log("Part 1", mostCalories);
+  log('Part 1', mostCalories);
 }
 
 export function Day1Part2() {
   // let inp = help.readShortInputByDay(1);
-  let inp = help.readInputByDay(1);
+  const inp = help.readInputByDay(1);
 
-  let elvesCalories = inp.split("\n\n");
+  let elvesCalories = inp.split('\n\n');
 
   elvesCalories = elvesCalories.map(groupElfCalories);
 
-  elvesCalories = elvesCalories.sort((a, b) => a - b).slice(-3)
-  
-  log("Part 2", elvesCalories.reduce((a, b) => a + b));
-}
+  elvesCalories = elvesCalories.sort((a, b) => a - b).slice(-3);
 
-let groupElfCalories = (caloriesString) => {
-  let allCalories = caloriesString.split("\n");
-  let allCaloriesNums = allCalories.map((x) => Number.parseInt(x));
-  let summedCalories = allCaloriesNums.reduce((acc, current) => acc + current);
-  return summedCalories;
-};
+  log('Part 2', elvesCalories.reduce((a, b) => a + b));
+}
--- AOC2022/index.js
+++ AOC2022/index.js
@@ -1,4 +1,8 @@
-import * as Day1Part1 from './Day_1/main.js';
+import * as Day1 from './Day_1/main.js';
+import * as Day2 from './Day_2/main.js';
 
-Day1Part1.Day1Part1()
-Day1Part1.Day1Part2()
+// Day1.Day1Part1()
+// Day1.Day1Part2()
+
+Day2.Day1Part1();
+Day2.Day1Part2();
